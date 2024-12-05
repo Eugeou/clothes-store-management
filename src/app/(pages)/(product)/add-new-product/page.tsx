d77309ce8f75f2ea6dda01db8bf50e7a148285bf
Eugeou <21522495@gm.uit.edu.vn>
2024-12-05 23:27:06 +0700

    Create page Add new product

---
+++ src/app/(pages)/(product)/add-new-product/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ImagePlus, PackageOpen } from 'lucide-react';
+import dynamic from 'next/dynamic';
+
+//Dynamic import AddNewProduct component
+const AddNewProduct = dynamic(() => import('@/components/ui-components/product/AddNewProduct'), {
+  //ssr: false
+});
+
+const AddProductPage: React.FC = () => {
+  return (
+    <div className=" flex p-4 bg-white shadow-2xl border border-gray-200 h-full w-full rounded-3xl">
+      <div style={{ flex: 2 }}>
+        <div className="flex space-y-0 mb-6 ml-0 border border-gray-300 space-x-2 items-center bg-white rounded-xl shadow-xl w-1/3 h-12"> 
+            <PackageOpen  className="ml-5 flex text-lg font-bold text-center text-indigo-600" />
+            <h3 className="space-y-0 font-semibold">Add product information</h3>
+        </div>
+        <AddNewProduct />
+      </div>
+      
+    </div>
+  );
+};
+
+export default AddProductPage;
